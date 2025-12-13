7463cbd52d528fcf5ebe6cf12dd898f5588a3807
Rajkrishna T R <rajkrishna.thevalil@gmail.com>
2025-12-13 18:55:26 +0530

    Refactor Rx_module with updated comments and signals
    Refactor Rx_module to improve clarity and functionality. Updated comments and signal handling.

--- RX.v
+++ RX.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: RAJKRISHNA T R
@@ -9,7 +9,9 @@
 // Project Name: UART
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: 1. Unless reset the data_out register holds the last value it received
+//              2. Protection feature needed to be implemented at stop bit state and other states if required
+//              3. Implement clock gating ?? 
 // 
 // Dependencies: 
 // 
@@ -19,20 +21,15 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-/* 
-     ----- ISSUES -----
 
-1. The reset signals fighting eachother 
-2. The timing management with the baud rate count and things need to be checked
-
-
-*/
+//-------------------------------------------------------------------------------------------------------------
+// Frequency 25MHz
+// Baud rate 115200
+//-------------------------------------------------------------------------------------------------------------
 
 
 
-
-
-
+//-------------------------------------------------------------------------------------------------------------
 
 // REF---> nandland,All about VLSI youtube channel
 
@@ -40,27 +37,52 @@
 //[ [START bit low] [-- 8 bit data --] [STOP bit HIGH] ] Total 11 bit frame
 // No parity bit included
 
+//-------------------------------------------------------------------------------------------------------------
+
 module Rx_module(
+                        
                     input wire clk,                    // CLOCK
                     input wire Rx_serial,              // RX_serial line 
-                    input wire Rx_en,                  // Rx_en from the baud rate generator
                     input wire MASTER_RST_BAR,         // reset everything
-                    input wire  data_rdy_clr,          // Clear the data ready indication flag - overwrites the value set by the module
-                    output wire data_rdy_flag,               // data ready indication flag
+                    input wire rdy_clr,              // Clear the data ready flag
+                    output wire data_rdy_flag,         // data ready indication flag
                     output reg[7:0] data_out,          // Data output
-                    output wire reset_rx_sample_count_flag   // Signal to baud rate generator reset the count for RX (Sample count)
-                 // output reg reset_tx_sample_count   // Signal to baud rate generator reset the count for TX
-    );
+                    output reg stop_bit_detc           // Stop bit detected
+
+                );
 
 
+//-----------------------------------------------------------------------------------------------------------------------------------
+// Baud rate generator controll part (Inbuilt in Rx module) // 
+
+parameter factor = 1; //usually taken factor 16,current value will be changed later
+    
+parameter inputclk_freq=25000000; // unit Hz (25MHz clock as input)
+    
+parameter Baud_rate=115200;// unit bps
+
+localparam Rx_en_count = inputclk_freq/(Baud_rate*factor); // calculating the suitable size for the Rx_en_count
+ 
+localparam  num_ff_Rx_en= $clog2(Rx_en_count+1); // number of flip flops needed
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
 
     reg [2:0] curr_state;            // Register to store the current state 
     reg [7:0] data_frm;              // Data byte is stored in this temporary register
-    reg data_rdy;
-    reg reset_rx_sample_count;       
+    reg data_rdy;                    // Indicating the received data is ready to be read
+         
+    parameter BIT_INDEX_SIZE=3; // No of bits needed to represet the data byte
+    reg [num_ff_Rx_en-1:0]sample_count;           // Used to sample the data from at the middle of the bit 
+    reg [BIT_INDEX_SIZE-1:0] Bit_index_count;     // To count the index while receiving the data bits
 
-  //  reg [4:0]sample_count;           // Used to sample the data from at the middle of the bit 
-    reg [3:0] Bit_index_count;       // To coutn the index while receiving the data bits
+   
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+                // STATE definitions //
+
 
     parameter IDLE_ST = 3'b000;          // Stay idle until the start bit falling transition received
     parameter START_BIT_ST = 3'b001;     // When start bit detetced sample the start bit
@@ -68,8 +90,15 @@ module Rx_module(
     parameter STOP_BIT_ST = 3'b011;      // Stop bit is sampled and move to the end state 
     parameter END_ST = 3'b100;           // Move the data from the register to the output register and also enable the ready signal
 
-    assign data_rdy_flag = (data_rdy) && (~(data_rdy_clr)); //  The flag that shows whether the data is ready to be read
-    assign reset_rx_sample_count_flag = (reset_rx_sample_count) || (1'b0); // Default value is zero but it can b reset by the state machine
+//----------------------------------------------------------------------------------------------------------------------------------
+                 // Continous assignments // 
+
+//assign data_rdy_flag = (data_rdy) && (~(rdy_clr)); //  The flag that shows whether the data is ready to be read
+// Ready flag cleared by the data_rdy inbuilt method and externally cleared by the reader module using rdy_clr input
+
+// assign reset_rx_sample_count_flag = (reset_rx_sample_count) || (1'b0); // Default value is zero but it can b reset by the state machine
+
+//-----------------------------------------------------------------------------------------------------------------------------------
 
     always@(posedge clk)  // Same clock for the rx, tx and baud rate generator
         begin
@@ -87,35 +116,36 @@ module Rx_module(
 
                     Bit_index_count<=3'd0;       // Bit index count cleared
 
-                    reset_rx_sample_count<=1'b1; // reset the count for RX
+                    sample_count<=0;             // Sampling count reset
 
-                 //   reset_tx_sample_count<=1'b1; // reset the count for TX
-
+                    stop_bit_detc<=1'b0;            // stop bit detected
             
-
                 end
                     
             else 
                 begin
 
-                       
-                 
-                    
-
+                  if(rdy_clr)
+                        begin
+                            data_rdy<=1'b0;
+                        end          
+                   
                    case(curr_state)
 
                     // ----  IDLE state  ---- // 
 
                         IDLE_ST:
+
                             begin
                                 
-                             //    reset_rx_sample_count<=1'b1; // Clear the RX enable count (Sample count)
-
-                             //         sample_count<=4'd0;    // Clear the sample count
+                          
+                                sample_count<=0;    // Clear the sample count
 
                                 Bit_index_count<=3'd0; //  clear the bit index count
 
-                                data_rdy<=1'b0;    // Clear the data ready flag
+                             //   data_rdy<=1'b0;    // Clear the data ready flag
+
+                                stop_bit_detc<=1'b0;  //  Stop bit detected flag
 
 
                                 if(Rx_serial==1'b0)
@@ -123,7 +153,7 @@ module Rx_module(
                                     begin
 
                                         curr_state<=START_BIT_ST;  // Rx_serial line showing '0' indicates first start bit
-                                        reset_rx_sample_count<=1'b1; // start sampling the start bit
+
 
                                     end
 
@@ -131,28 +161,27 @@ module Rx_module(
 
                                     begin
 
-                                        curr_state<=IDLE_ST;       // Until start bit is detected the stay in the IDLE state
+                                        curr_state<=IDLE_ST;  // Until start bit is detected the stay in the IDLE state
 
                                     end
 
                             
                             end
                     
-                    // ----  START_BIT_STATE  ----//
+                    // ----  START_BIT_STATE  ---- //
 
-                        START_BIT_ST:  // This state double checks if thhe Rx_serial is giving start bit or not, by checking it by sampling 
+                        START_BIT_ST:  // This state double checks if thhe Rx_serial is giving start bit or not,
+                                       // by checking it by sampling 
                                        // and decide the next state based on the sampling result
 
 
                             begin
 
+                                
                                 // reset_rx_sample_count<=1'b0;
 
-                                if(Rx_en==1'b1)  // Middle sample signal from the baud rate generator
+                                if(sample_count>=((Rx_en_count-1)/2)-1)  // Middle sample signal to be sent from the baud rate generator
                                             begin
-                                                
-                                                 
-                                       //         sample_count<=sample_count+1'b1; // ? Increment the sample count when Rx_en signal is high
 
                                                 if (Rx_serial==1'b0)   // Start bit is confirmed
 
@@ -160,53 +189,55 @@ module Rx_module(
 
                                                         curr_state<=DATA_BYTE_ST;  // Go to data sampling state
 
-                                                        reset_rx_sample_count<=1'b1;
+                                                        sample_count<=0;           // Reset the sample count
 
-                                          //              sample_count<=0;           // ? reset the sample count since start bit sampling done
+                                                        // reset the sample count since start bit sampling done
                                                         // reset signal might be to send to the baud rate generator
-
+                                                                                                                           
                                                     end
                                                 else
+
                                                     begin
 
-                                                        curr_state<=IDLE_ST;         
-                                                           // Rx_serial line is not pulled down so not start bit
-                                                        // Double checking failed so return back to idle state
+                                                        curr_state<=IDLE_ST;       
 
-                                                    end
-                                        
+                                                        // Rx_serial line is not pulled down so not start bit
+                                                        // Double checking failed so return back to idle state
+                                                        
+
+                                                    end             
 
                                             end
                                 else 
                                         begin
+
                                                 curr_state<=START_BIT_ST;
 
-                                               
+                                                sample_count<=sample_count+1;
 
-                                                // Increment the sampling count here since waiting
+                                                //increment the sampling count here since waiting
+                                       
                                         end
                                                                       
                             end
+
                         // ---- DATA_BYTE_STATE ---- //
+
                      DATA_BYTE_ST:
                                     begin
 
-                                   //      reset_rx_sample_count<=1'b0;
                                         
-                                   //      sample_count<=sample_count+1; // ??This should be in the baud rate module or should be in the top of this module?
-
-                                        if(Rx_en==1'b1)
+                                       
+                                        
+                                        if(sample_count>=((Rx_en_count-1)/2))
 
                                             begin
 
                                                 data_frm[Bit_index_count]<=Rx_serial; // Read the serial line and 
 
-                              // store data in the corresponding index position of the temporary register
-                              // sample_count<=4'd0;    -- >   reset the sample count               
-                              
-
-                                                reset_rx_sample_count<=1'b1; // reset sampling count
-
+                                                // store data in the corresponding index position of the temporary register
+                                                sample_count<=0;  //  -- >   reset the sample count               
+                            
                                                 Bit_index_count<=Bit_index_count+1'b1; // reset the Bit index count after each bit sampled successfully
                                             
                                         
@@ -220,64 +251,86 @@ module Rx_module(
                                                     end
                                                 else 
                                                     begin
-                                                        curr_state<=STOP_BIT_ST;           
-                                                        //  Data byte receiving done
-                                                                                            
-                                                                                            // Go to stop state
+                                                               
+                                                        //Data byte receiving done
                                                         Bit_index_count<=0;
+                                                                                            
+                                                        // Go to stop state
+                                                        curr_state<=STOP_BIT_ST;  
+
+
                                                     end 
+                                            end
+
+                                        else 
+                                            
+                                            begin
+                                                    sample_count<=sample_count+1;
+
+                                                    curr_state<=DATA_BYTE_ST;
+
                                             end
 
                                     end
 
                         // ---- STOP_BIT_STATE ---- //
-                    STOP_BIT_STATE:
-                                    begin
-                                   //     reset_rx_sample_count<=1'b0;
-                                   //     sample_count<=sample_count+1;
 
-                                        if(Rx_en==1'b1)
+                    STOP_BIT_ST:
+                                    begin
+                                   
+                                       
+                                        if(sample_count>=((Rx_en_count-1)/2)-1) 
 
                                             begin
 
-                                               if(Rx_serial==1'b1) // Stop bit detected
+                                                        curr_state<=END_ST;     // go to end state
 
-                                                    begin
+                                                        sample_count<=0;        // sample count
 
-                                                        curr_state<=END_ST;           
+                                                        stop_bit_detc<=1'b1;    // stop bit detected
+
                                                         
-                                //         Automatically zero since the count reaches the maximum value  sample_count<=4'd0;
 
-                                                    end
+                                             end
 
-                                                else 
+                                            else 
 
-                                                    begin
+                                                begin
 
-                                                        curr_state<=STOP_BIT_ST;
+                                                        stop_bit_detc<=1'b0;           //  Stop bit not detected
+                                                        
+                                                        curr_state<=STOP_BIT_ST;       // Stay until the stop bit is sampled 
 
-                                                    end
+                                                        // FEATURE IF STOP BIT NOT DETECTED EVEN IF TIMER OVERFLOWS
+                                                        // Implement the protection 
+                                                        // Abort the process or something will do
 
-                                            end                  
+                                                        sample_count<=sample_count+1;  // Increment the sample counter
 
-                                    end
+                                                end           
+
+                                     end                  
+
+                                
 
                         // ---- END STATE ---- //
 
                     END_ST:
                                 begin
 
-                                    curr_state<=IDLE_ST;
+                                    curr_state<=IDLE_ST;  // Go to IDLE state 
 
-                                    data_out<=data_frm;
+                                    data_out<=data_frm;   // Pass the data byte sampled to the data_out reg
 
-                                    data_rdy<=1'b1;
+                                    data_rdy<=1'b1;       // Enable the data ready flag
+
+                                    data_frm<=8'b0;       // Data frame reset
 
                                 end
                     default: 
                                 begin
 
-                                    curr_state<=IDLE_ST;
+                                    curr_state<=IDLE_ST;  //  set Current state IDLE state by default
 
                                 end         
                                     
